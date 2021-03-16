e6631442e6f723cd0757735f9709daff5f72df7e
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2021-03-15 22:08:17 -0500

    more fun

--- main.tf
+++ main.tf
@@ -7,13 +7,22 @@ terraform {
   }
 }
 
+provider "aws" {
+    region = "us-east-1"
+    access_key = "access_key_here"
+    secret_key = "secret_key_here"
+}
+
 # resource "<provider>_<resource_type>" "name" {
 #     config options...
 #     key = "value"
 #     key2 = "another_value"
 # }
 
-resource "aws_instance" "my-first-ec2-server" {
-  ami           = "ami-042e8287309f5df03"
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "my-first-ec2-server" {
+#   ami           = "ami-042e8287309f5df03"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "ubuntu"
+#   }
+# }
